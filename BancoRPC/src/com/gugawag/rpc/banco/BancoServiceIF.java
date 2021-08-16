e9539b84b471fe24ec10ce8a543506958d009cb1
andreia23 <44982439+andreia23@users.noreply.github.com>
2021-08-16 01:50:19 -0300

    Add files via upload

---
+++ BancoRPC/src/com/gugawag/rpc/banco/BancoServiceIF.java
@@ -0,0 +1,13 @@
+package com.gugawag.rpc.banco;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface BancoServiceIF extends Remote {
+
+    double saldo(String conta) throws RemoteException;
+    int quantidadeContas() throws RemoteException;
+    void adicionarConta() throws RemoteException;
+	void removerConta(String numero) throws RemoteException;
+
+}
