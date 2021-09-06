464a0bd911eaa4799af419375ae87db04d291e0a
andreia23 <44982439+andreia23@users.noreply.github.com>
2021-09-06 12:36:11 -0300

    Add files via upload

---
+++ rabbitmq-basico/src/main/java/ifpb/pdist/andreia/NewTask.java
@@ -0,0 +1,33 @@
+package ifpb.pdist.andreia;
+
+import java.util.Scanner;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class NewTask {
+
+	private static final String TASK_QUEUE_NAME = "task_queue";
+
+	public static void main(String[] argv) throws Exception {
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		connectionFactory.setHost("localhost");
+		connectionFactory.setUsername("andreia");
+		connectionFactory.setPassword("ifpb");
+		try (Connection connection = connectionFactory.newConnection(); Channel channel = connection.createChannel()) {
+			channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+			System.out.println("Escreva um texto: ");
+			Scanner messageTexto = new Scanner(System.in);
+
+			String mensagem = messageTexto.nextLine();
+
+			channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN,
+					mensagem.getBytes("UTF-8"));
+			System.out.println("[x] Enviado '" + mensagem + "'");
+		}
+	}
+
+}
