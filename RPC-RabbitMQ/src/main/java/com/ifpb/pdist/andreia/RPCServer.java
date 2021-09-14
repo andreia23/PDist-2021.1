00eed9ef39ae5c022a4751641710e9ae49d1941b
andreia23 <44982439+andreia23@users.noreply.github.com>
2021-09-14 00:10:58 -0300

    Add files via upload

---
+++ RPC-RabbitMQ/src/main/java/com/ifpb/pdist/andreia/RPCServer.java
@@ -0,0 +1,72 @@
+package com.ifpb.pdist.andreia;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class RPCServer {
+
+	private static final String RPC_QUEUE_NAME = "rpc_queue";
+
+	private static String sendName(String name) {
+		return name;
+
+	}
+
+	public static void main(String[] argv) throws Exception {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		factory.setUsername("andreia");
+		factory.setPassword("ifpb");
+
+		try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+			channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+			channel.queuePurge(RPC_QUEUE_NAME);
+
+			channel.basicQos(1);
+
+			System.out.println(" [x] Awaiting RPC requests");
+
+			Object monitor = new Object();
+			DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+				AMQP.BasicProperties replyProps = new AMQP.BasicProperties.Builder()
+						.correlationId(delivery.getProperties().getCorrelationId()).build();
+
+				String response = "";
+
+				try {
+					String message = new String(delivery.getBody(), "UTF-8");
+
+					response = sendName(message);
+					System.out.println("Nome: " + message);
+
+				} catch (RuntimeException e) {
+					System.out.println(" [.] " + e.toString());
+				} finally {
+					channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps,
+							response.getBytes("UTF-8"));
+					channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+					// RabbitMq consumer worker thread notifies the RPC server owner thread
+					synchronized (monitor) {
+						monitor.notify();
+					}
+				}
+			};
+
+			channel.basicConsume(RPC_QUEUE_NAME, false, deliverCallback, (consumerTag -> {
+			}));
+			// Wait and be prepared to consume the message from RPC client.
+			while (true) {
+				synchronized (monitor) {
+					try {
+						monitor.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+}
